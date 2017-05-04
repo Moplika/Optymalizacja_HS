ff56dc5bba754741f66733d2a3dfe2f1becd5df9
Moplika <monika94lubin@gmail.com>
2017-05-04 22:17:04 +0200

    Może tym razem wszystko się zapisało...

---
+++ HarmonySearch/Expression.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class Expression
+{
+public:
+	Expression();
+	Expression(std::string string);
+	~Expression();
+
+	virtual double calculate(std::vector<double> x);
+};
+
